a66e677357f7cd46d82bb437a732fba185542f25
ryyos <riodwi12174@gmail.com>
2025-03-19 16:07:47 +0700

    released: 0.0.1 stable

---
+++ README.md
@@ -0,0 +1,22 @@
+# CyLog
+
+![Version](https://img.shields.io/badge/version-0.0.1-green.svg?cacheSeconds=2592000)
+![ProjectImage](https://raw.githubusercontent.com/ryyos/ryyos/refs/heads/main/images/erine/erine.jpg)
+
+---
+
+## Features ✨
+
+- **informative log**: informative logs easy to understand and colorful
+- **flexible storage**: storage with customizable keys
+- **easy use**: Simple calls and easy setup
+
+## Requirement ⚙️
+
+- [click](https://pypi.org/project/click/) v8.1.8+
+
+## Installation 🛠️
+
+```sh
+pip install cylog
+```
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'library used to manage file queues'
+
+setup(
+    name="CyLog",
+    version=VERSION,
+    author="ryyos (Rio Dwi Saputra)",
+    author_email="<riodwi12174@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['click'],
+    keywords=['log', 'logger', 'queque'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
